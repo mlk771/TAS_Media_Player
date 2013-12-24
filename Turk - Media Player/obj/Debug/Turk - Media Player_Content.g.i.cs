92deb7f5de1ea0abee675b1df8b1d853b794b4f1
mlk771 <mlk_771@hotmail.com>
2013-12-24 16:09:39 +1000

    Fix: Media folder

--- Turk - Media Player/obj/Debug/Turk - Media Player_Content.g.i.cs
+++ Turk - Media Player/obj/Debug/Turk - Media Player_Content.g.i.cs
@@ -8,6 +8,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pause.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/close.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/max.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/media2.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/minimize.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/open.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/pause.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/play.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("media/stop.png")]
 
 
